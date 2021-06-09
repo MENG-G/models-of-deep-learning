b9d2b6e765f2d7c1ce6300475be37151c852cd90
Meng Guo <kakumougm@gmail.com>
2021-06-10 00:00:21 +0900

    resnet

--- Resnet.py
+++ Resnet.py
@@ -1,11 +1,9 @@
-from _typeshed import IdentityFunction
-from os import name
 import torch
 import torch.nn as nn
 from torch.hub import load_state_dict_from_url
-from torch.nn.modules.conv import Conv2d
 
 
+model_urls = {}
 
 
 def conv3x3(in_planes, out_planes, stride=1, padding=1):
@@ -17,7 +15,6 @@ def conv1x1(in_planes, out_planes, stride=1):
 
 
 class BasicBlock(nn.Module):
-
     expansion = 1
     def __init__(self, in_planes, planes, stride=1, downsample=None, norm_layer=None):
         super(BasicBlock, self).__init__()
@@ -45,24 +42,22 @@ class BasicBlock(nn.Module):
         
         out += identity
         out = self.relu(out)
-    
+
 
 class BottleNeck(nn.Module):
-
     expansion = 4
-    def __init__(self, in_planes, planes, stride=1, downsample=None, norm_layer=None):
+    def __init__(self, in_planes, planes, stride=1, downsample=None):
         super(BottleNeck, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
     
         self.conv1 = conv1x1(in_planes, planes)
-        self.bn1 = norm_layer(planes)
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = conv3x3(planes, planes, stride)
-        self.bn2 = norm_layer(planes)
+        self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = conv1x1(planes, planes * self.expansion)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
-        self.sride = stride
+        self.sride = stride      
 
     def forward(self, x):
         identity = x
@@ -79,7 +74,7 @@ class BottleNeck(nn.Module):
         out = self.bn3(out)
 
         if self.downsample is not None:
-            identity = self.downsample
+            identity = self.downsample(x)
         
         out += identity
         out = self.relu(out)
@@ -88,65 +83,66 @@ class BottleNeck(nn.Module):
 
 
 class Resnet(nn.Module):
-
-    def __init__(self, block, layer, num_class=100, norm_layer=None):
+    def __init__(self, block, layers, num_class=100):
         super(Resnet, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        
+        # 首先使用7x7的kernel进行卷积，stride=2将图片长宽变为一半
         self.in_planes = 64
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = norm_layer(self.in_planes)
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # 所有的resnet都进行的操作到此完成
+        # 接下来进行4个stage
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        # 全局平均池化
         self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        # 全连接
         self.fc = nn.Linear(512*block.expansion, num_class)
 
+        # 模型初始化
         for m in self.modules():
-            if isinstance(m, nn,Conv2d):
+            if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-            
-        def _make_layer(self, block, planes, blocks, stride=1):
-            norm_layer = self._norm_layer
-            downsample = None
 
-            if stride != 1 or self.inplanes != planes*block.expansion:
-                downsample = nn.Sequential(
-                    conv1x1(self.inplanes, planes*block.expansion, stride),
-                    norm_layer(planes*block.expansion)
-                )
-            
-            layers = []
-            layers.append(block(self.inplanes, planes, stride, downsample, norm_layer))
-            self.inplanes = planes * self.expansion
-            for _ in range(1, blocks):
-                layers.append(block(self.inplanes, planes, norm_layer=norm_layer))
+    # _make_layer(self, block种类, 输出通道数, block数量，stride)
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
 
-            return nn.Sequential(*layers)
+        if stride != 1 or self.in_planes != planes*block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.in_planes, planes*block.expansion, stride),
+                nn.BatchNorm2d(planes*block.expansion)
+            )
         
-        def forward(self, x):
-            x = self.conv1(x)
-            x = self.bn1(x)
-            x = self.relu(x)
-            x = self.maxpool(x)
+        layers = []
+        layers.append(block(self.in_planes, planes, stride, downsample))
+        self.in_planes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_planes, planes))
 
-            x = self.layer1(x)
-            x = self.layer2(x)
-            x = self.layer3(x)
-            x = self.layer4(x)
+        return nn.Sequential(*layers)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
 
-            x = self.avgpool(x)
-            x = torch.flattern(x, 1)
-            x = self.fc(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
 
-            return x
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
 
 
 def _resnet(arch, block, layers, pretrained, progress, **kwargs):
@@ -159,10 +155,13 @@ def _resnet(arch, block, layers, pretrained, progress, **kwargs):
 
 def Resnet152(pretrained=False, progress=True, **kwargs):
     return _resnet('resnet152', BottleNeck, [3, 8, 36, 3], pretrained, progress, **kwargs)
+    # return _resnet('resnet152', ResnetBottleneck, [3, 8, 36, 3], pretrained, progress, **kwargs)
 
 
 
 if __name__ == '__main__':
-    model = Resnet152(pretrained=False)
-
+    from torchsummary import summary
+    model = Resnet152(pretrained=False, progress=True)
+    # 最后使用了全局平均池化，所以输入图片的尺寸没有影响
+    summary(model, (3, 224, 224))
 
