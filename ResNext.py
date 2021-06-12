b11633be4dbe5882b29cfcfa13434f90e2de6332
MENG-G <kakumougm@gmail.com>
2021-06-12 23:25:06 +0900

    upload

--- ResNext.py
+++ ResNext.py
@@ -1,13 +1,16 @@
+from turtle import width
 import torch
 import torch.nn as nn
 
-def conv3x3(inchannels, outchannels, stride=1, padding=1):
+
+def conv3x3(inchannels, outchannels, stride=1, padding=1, groups=1):
     return nn.Conv2d(
         in_channels=inchannels, 
         kernel_size=3,
         out_channels=outchannels,
         stride=stride,
         padding=padding,
+        groups=groups,
         bias=False
     )
 
@@ -22,38 +25,18 @@ def conv1x1(inchannels, outchannels, stride=1):
     )
 
 
-class BasicBlock(nn.Module):
-    expansion = 1
-    def __init__(self, inchannels, outchannels, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inchannels, outchannels, stride)
-        self.bn1 = nn.BatchNorm2d(outchannels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(outchannels, outchannels)
-        self.bn2 = nn.BatchNorm2d(outchannels) 
-        self.downsample = downsample
-
-    def forward(self, x):
-        identity = x
-        x = self.relu(self.bn1(self.conv1(x)))
-        x = self.bn2(self.conv2(x))
-        if self.downsample is not None:
-            identity = self.downsample(identity)
-        x += identity
-        x = self.relu(x)
-        return x
-
-
 class BottleNeck(nn.Module):
     expansion = 4
-    def __init__(self, inchannels, outchannels, stride=1, downsample=None):
+    def __init__(self, inchannels, outchannels, stride=1, downsample=None, groups=1, width_per_group=64):
         super(BottleNeck, self).__init__()
-        self.conv1 = conv1x1(inchannels, outchannels)
-        self.bn1 = nn.BatchNorm2d(outchannels)
+        width = int(outchannels * (width_per_group / 64.)) * groups
+
+        self.conv1 = conv1x1(inchannels, width)
+        self.bn1 = nn.BatchNorm2d(width)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(outchannels, outchannels, stride)
-        self.bn2 = nn.BatchNorm2d(outchannels)
-        self.conv3 = conv1x1(outchannels, outchannels*self.expansion)
+        self.conv2 = conv3x3(width, width, stride, groups=groups)
+        self.bn2 = nn.BatchNorm2d(width)
+        self.conv3 = conv1x1(width, outchannels*self.expansion)
         self.bn3 = nn.BatchNorm2d(outchannels*self.expansion)
         self.downsample = downsample         
 
@@ -70,10 +53,12 @@ class BottleNeck(nn.Module):
         return x
 
 
-class Resnet(nn.Module):
-    def __init__(self, block, block_nums, num_classes=1000):
-        super(Resnet, self).__init__()
+class ResNext(nn.Module):
+    def __init__(self, block, block_nums, groups=1, width_per_group=64, num_classes=1000):
+        super(ResNext, self).__init__()
         self.inchannels = 64
+        self.groups = groups
+        self.width_per_group = width_per_group        
         self.conv1 = nn.Conv2d(3, self.inchannels, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(self.inchannels)
         self.relu = nn.ReLU(inplace=True)
@@ -101,10 +86,10 @@ class Resnet(nn.Module):
                 nn.BatchNorm2d(outchannels*block.expansion)
             )
         layers = []
-        layers.append(block(self.inchannels, outchannels, stride, downsample))
+        layers.append(block(self.inchannels, outchannels, stride, downsample, groups=self.groups, width_per_group=self.width_per_group))
         self.inchannels = outchannels * block.expansion
         for _ in range(1, blocks):
-            layers.append(block(self.inchannels, outchannels))
+            layers.append(block(self.inchannels, outchannels, groups=self.groups, width_per_group=self.width_per_group))
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -119,23 +104,13 @@ class Resnet(nn.Module):
         return x
 
 
-def Resnet152():
-    return Resnet(BottleNeck, [3, 8, 36, 3])
+def ResNext50_32x4d():
+    return ResNext(BottleNeck, [3, 4, 6, 3], groups=32, width_per_group=4)
 
-def Resnet101():
-    return Resnet(BottleNeck, [3, 4, 23, 3])
-
-def Resnet50():
-    return Resnet(BottleNeck, [3, 4, 6, 3])
-
-def Resnet34():
-    return Resnet(BasicBlock, [3, 4, 6, 3])
-
-def Resnet18():
-    return Resnet(BasicBlock, [2, 2, 2, 2])
-
+def ResNext101_32x8d():
+    return ResNext(BottleNeck, [3, 4, 23, 3], groups=32, width_per_group=8)
 
 if __name__ == "__main__":
-    model = Resnet34()
+    model = ResNext101_32x8d()
     x = torch.randn(1, 3, 224, 224)
     print(model(x).shape)
